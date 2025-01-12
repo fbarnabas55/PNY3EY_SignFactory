fb703565ca1011631c738cc47f31dccce9a8cd1e
fenyvesi.barnabas55@stud.uni-obuda.hu <fenyvesi.barnabas55@stud.uni-obuda.hu>
2025-01-12 17:37:58 +0100

    repo done

---
+++ SignFactory/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignFactory.Data;
+using SignFactory.Entities.Entity_Models;
+
+namespace SignFactory.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        SignFactoryDbContext cxt;
+
+        public OrderController(SignFactoryDbContext cxt)
+        {
+            this.cxt = cxt;
+        }
+
+        [HttpPost]
+        public void addOrder(Order order)
+        {
+            cxt.Orders.Add(order);
+            cxt.SaveChanges();
+        }
+    }
+}
